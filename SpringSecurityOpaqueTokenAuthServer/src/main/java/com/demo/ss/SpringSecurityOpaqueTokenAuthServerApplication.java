5a32cb47e1fe1d8bbbc5b8c8e66920363b40854f
bsah790 <62319267+bsah790@users.noreply.github.com>
2022-02-01 11:55:11 +0530

    Add files via upload
    Opaque token introspect implementation

---
+++ SpringSecurityOpaqueTokenAuthServer/src/main/java/com/demo/ss/SpringSecurityOpaqueTokenAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOpaqueTokenAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOpaqueTokenAuthServerApplication.class, args);
+	}
+
+}
---
+++ SpringSecurityOpaqueTokenResServer/src/test/java/com/demo/SpringSecurityOpaqueTokenResServer/SpringSecurityOpaqueTokenResServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.SpringSecurityOpaqueTokenResServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOpaqueTokenResServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
